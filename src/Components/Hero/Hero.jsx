af09710de606fce42d789ba392ede11f717764bd
wdvaibhav15 <wdvaibhav15@gmail.com>
2026-04-01 16:35:20 +0530

    complete hero section

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
-import './Hero.css'
+import React from "react";
+import "./Hero.css";
+import dark_arrow from "../../assets/dark-arrow.png";
 
 const Hero = () => {
   return (
-    <div>
-      
+    <div className="hero container">
+      <div className="hero-text">
+        <h1>We Ensure better education for a better world</h1>
+        <p>
+          Our Cutting-edge curriculam is degined to empower students with the
+          knowledge , skills and experiences needed to excel in the dynamic
+          fielfs of education and career.
+        </p>
+        <button className="btn">Learn Moreimg <img src={dark_arrow} alt="" /></button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
