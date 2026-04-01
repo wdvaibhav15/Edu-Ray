dd455d37c3576fe94da3d61191d99f41faedbbdd
wdvaibhav15 <wdvaibhav15@gmail.com>
2026-04-01 17:47:43 +0530

    completed about section

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './About.css'
+import about from '../../assets/about.png'
+import play_icon from '../../assets/play-icon.png'
+
 
 const About = () => {
   return (
     <div className="about">
-      <div className="about-left"></div>
+      <div className="about-left">
+        <img src={about} alt="" className="about-img" />
+        <img src={play_icon} alt="" className="play-icon" />
+      </div>
+      <div className="about-right">
+        <h3>ABOUT UNIVERSITY</h3>
+        <h2>Nurturing Tomorrow's Leaders Today</h2>
+        <p>Embark on a transformative educational journey with our university's comprehensive educational prohgrams. Our cutting-edge curriculum is designed to empower students with the knowledge, skills and experiences needed to excel in the dynamic fields of education.</p>
+        <p>With a focus on innovative , hand-on learning, and personalized mentorship , our programs prepare aspiring educators to make a meaningful ipact in classrooms, school and communication.</p>
+        <p>whether you aspire to become a teacher , administrator, counselor , or educational leader , our diverse range of programs offers the perfect pathway to achieve your goals and unlock your full potential in shaping the future of education.</p>
+      </div>
     </div>
   )
 }
