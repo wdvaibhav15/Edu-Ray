5787e5e48f189cddd5324c3b792638a9abd5d061
wdvaibhav15 <wdvaibhav15@gmail.com>
2026-04-01 15:59:46 +0530

    navbar completed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,15 +4,15 @@ import logo from '../../assets/logo.png'
 
 const Navbar = () => {
   return (
-    <nav>
-      <img src={logo} alt="" />
+    <nav className="container">
+      <img src={logo} alt=""  className="logo"/>
       <ul>
         <li>Home</li>
         <li>Program</li>
         <li>About Us</li>
         <li>Campus</li>
         <li>Testimonials</li>
-        <li>Contact Us</li>
+        <li><button className="btn">Contact Us</button></li>
       </ul>
     </nav>
   )
