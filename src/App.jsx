601ca3cfbff804567ee099498997243168a2fe49
wdvaibhav15 <wdvaibhav15@gmail.com>
2026-04-01 23:26:40 +0530

    contact section completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Title from './Components/Title/Title'
 import About from './Components/About/About'
 import Campus from './Components/Campus/Campus'
 import Testimonial from './Components/Testimonials/Testimonial'
+import Contact from './Components/Contact/Contact'
 
 const App = () => {
   return (
@@ -21,6 +22,7 @@ const App = () => {
         <Title subTitle="TESTIMONIALS" title="what Student Says?" />
         <Testimonial />
         <Title subTitle="Contact Us" title="Get in Touch!" />
+        <Contact />
       </div>
       </div>
   )
--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,25 +1,66 @@
+// b47fe485-e14a-4b1b-9945-437298f2a8d4
 import React from 'react'
 import './Contact.css'
+import msg_icon from '../../assets/msg-icon.png'
+import mail_icon from '../../assets/mail-icon.png'
+import phone_icon from '../../assets/phone-icon.png'
+import location_icon from '../../assets/location-icon.png'
+import white_arrow from '../../assets/white-arrow.png'
 
 const Contact = () => {
+    const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "b47fe485-e14a-4b1b-9945-437298f2a8d4");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+};
   return (
     <div className='contact'>
       <div className="contact-col">
-        <h3>Send us a message</h3>
+        <h3>Send us a message <img src={msg_icon} alt="" /></h3>
         <p> Feel free to reach out through contact form or find our contact
             information below. Your feedback, questions, and suggestions are
             important to us as we strive to provide exceptional service to our
             university community.</p>
             <ul>
-                <li>ContactEduRay@.com</li>
-                <li>+91 912-345-6789</li>
-                <li>Edu-Ray College
+                <li> <img src={mail_icon} alt="" /> ContactEduRay@.com</li>
+                <li> <img src={phone_icon} alt="" /> +91 912-345-6789</li>
+                <li> <img src={location_icon} alt="" /> Edu-Ray College
                     45 Academic Street,
                     Downtown Education District,
                     London, UK - EC1A 1BB</li>
             </ul>
       </div>
-      <div className="contact-col"></div>
+      <div className="contact-col">
+        <form  onSubmit={onSubmit}>
+            <lebel>Your Name</lebel>
+            <input type="text" name="name" placeholder='Enter your name' required />
+            <lebel>Your Phone Number</lebel>
+            <input type="tell" name="phone" placeholder='Enter your Phone Number' required />
+            <lebel>Message </lebel>
+            <textarea name ="message"  rows="6" placeholder='Enter your message here' required></textarea>
+            <button type='submit' className='btn dark-btn'>Send Message <img src={white_arrow} alt="" /></button>
+        </form>
+        <span>{result}</span>
+      </div>
     </div>
   )
 }
