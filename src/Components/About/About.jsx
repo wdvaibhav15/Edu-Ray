99d8afef90fd36df6e98a0f47fb21401f217dd47
wdvaibhav15 <wdvaibhav15@gmail.com>
2026-04-01 17:19:47 +0530

    initialize about section

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './About.css'
+
+const About = () => {
+  return (
+    <div className="about">
+      <div className="about-left"></div>
+    </div>
+  )
+}
+
+export default About
