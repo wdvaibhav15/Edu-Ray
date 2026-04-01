e8b0df851083340f7fb6eefc6607e81ea1c9852c
wdvaibhav15 <wdvaibhav15@gmail.com>
2026-04-01 17:06:25 +0530

    complete program section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
+import Programs from './Components/Programs/Programs'
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
-    </div>
+      <div className="container">
+        <Programs />
+      </div>
+      </div>
   )
 }
 
--- src/Components/Programs/Programs.jsx
+++ src/Components/Programs/Programs.jsx
@@ -1,20 +1,34 @@
 import React from 'react'
 import './Programs.css'
-import program1 from '../../assets/program1.jpg'
-import program2 from '../../assets/program2.jpg'
-import program3 from '../../assets/program3.jpg'
-
+import program1 from '../../assets/program-1.png'
+import program2 from '../../assets/program-2.png'
+import program3 from '../../assets/program-3.png'
+import program_icon1 from '../../assets/program-icon-1.png'
+import program_icon2 from '../../assets/program-icon-2.png'
+import program_icon3 from '../../assets/program-icon-3.png'
 const Programs = () => {
   return (
     <div className ="programs">
       <div className="program">
         <img src={program1} alt="" />
+        <div className="caption">
+            <img src={program_icon1} alt="" />
+            <p>Graduation Degree</p>
+        </div>
       </div>
       <div className="program">
         <img src={program2} alt="" />
+        <div className="caption">
+            <img src={program_icon2} alt="" />
+            <p>Master Degree</p>
+        </div>
       </div>
       <div className="program">
         <img src={program3} alt="" />
+        <div className="caption">
+            <img src={program_icon3} alt="" />
+            <p>Post Graduation</p>
+        </div>
       </div>
     </div>
   )
