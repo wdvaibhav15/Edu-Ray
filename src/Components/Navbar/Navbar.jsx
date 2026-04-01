51e6d14fa1c8deee6fe546933b607ff4071c2629
wdvaibhav15 <wdvaibhav15@gmail.com>
2026-04-01 15:39:40 +0530

    Initial commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../../assets/logo.png'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <nav>
+      <img src={logo} alt="" />
+      <ul>
+        <li>Home</li>
+        <li>Program</li>
+        <li>About Us</li>
+        <li>Campus</li>
+        <li>Testimonials</li>
+        <li>Contact Us</li>
+      </ul>
+    </nav>
   )
 }
 
